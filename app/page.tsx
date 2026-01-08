ffa56565072d2dc308bbc42b2a97ad648f639a54
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2026-01-08 22:45:06 +0530

    register

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+'use client';
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+
 // oRINmhD7lzjmj5sA
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       
